df3d37d64ce0bc69418b411191482a58c85a85e5
cleiton monteiro <cleitonmontsilva@gmail.com>
2018-11-12 20:19:05 -0300

    mudanca na estrutura, divisao em mais funcoes

--- dpll.cpp
+++ dpll.cpp
@@ -3,70 +3,93 @@
 #include <vector>
 #include <time.h>
 #include <stdio.h>
+#include <set>
 
 using namespace std;
 
-vector<vector<int>> simplifica(vector<vector<int>> formula) {
-    bool tem_clausula_unitaria = true;
-    int atomo_sozinho, i;
+
+int get_literal_clausula_unitaria( vector<vector<int>> formula ) {
+    int literal = 0;
+    
+    for( int i=0; i < formula.size(); i++ ) {
+        if( formula[i].size() == 1 ) {
+            literal = formula[i][0];
+            break;
+        }
+    }
+    return literal;
+}
+
+void remover_clausulas_com_literal( int literal, vector<vector<int>> &formula ) {
     vector<int>::iterator it;
     
-    while (tem_clausula_unitaria) {
-        atomo_sozinho = 0;
-        for (i=0; i < formula.size(); i++) {
-            if (formula[i].size() == 1) {
-                atomo_sozinho = formula[i][0];
-                break;
-            }
+    for( int i=0; i < formula.size(); i++ ) {
+        it = find( formula[i].begin(), formula[i].end(), literal );
+        if( it != formula[i].end() ) {
+            formula.erase(formula.begin()+i);
+            i--;
         }
+    }
+}
 
-        if (atomo_sozinho == 0) break;
+void remover_negacao_literal( int literal, vector<vector<int>> &formula ) {
+    vector<int>::iterator it;
 
-        for (i=0; i < formula.size(); i++) {
-            it = find(formula[i].begin(), formula[i].end(), atomo_sozinho);
-            if (it != formula[i].end()) {
-                formula.erase(formula.begin()+i);
-                i--;
-            }
+    for( int i=0; i < formula.size(); i++) {
+        it = find( formula[i].begin(), formula[i].end(), -literal );
+        
+        if( it != formula[i].end() ) {
+            formula[i].erase(it);
         }
+    }
+}
 
-        for (i=0; i < formula.size(); i++) {
-            it = find(formula[i].begin(), formula[i].end(), atomo_sozinho*-1);
-            if (it != formula[i].end()) {
-                formula[i].erase(it);
-            }
+bool contem_clausula_vazia( vector<vector<int>> formula ) {
+    for( int i=0; i < formula.size(); i++ ) {
+        if ( formula[i].size() == 0 ){
+            return true;
         }
+    }
+    return false;
+}
+
+vector<vector<int>> simplifica( vector<vector<int>> formula ) {
+    int literal, i;
+
+    while ( true ) {
+        literal = get_literal_clausula_unitaria( formula );
+        
+        if ( literal == 0 ) break;
+
+        remover_clausulas_com_literal( literal, formula );
+        remover_negacao_literal( literal, formula );
     }
     return formula;
 }
 
-bool dpll(vector<vector<int>> formula) {
+bool dpll( vector<vector<int>> formula ) {
     formula = simplifica(formula);
     
-    if (formula.empty()) return true;
-    
-    for (int i=0; i < formula.size(); i++) {
-        if (formula[i].size() == 0){
-            return false;
-        }
-    }
-    // usar heuristicas
-    // loop ??
+    if( formula.empty() ) return true;
+    if( contem_clausula_vazia( formula) ) return false;
+
     int literal = formula[0][0];
-    // cout << "Literal : " << literal << endl;
-    formula.push_back({literal});
-    
-    if (dpll(formula)) return true;
+
+    formula.push_back( { literal } );
+
+    if( dpll( formula ) ) {
+        return true;
+    }
     
     formula.pop_back();
-    formula.push_back({-literal});
+    formula.push_back( { -literal } );
     
-    if (dpll(formula)) return true;
+    if( dpll( formula ) ) return true;
     return false;
 }
 
 
-int main(int argc, char const *argv[]) {
+int main( int argc, char const *argv[] ) {
     clock_t tempo_inicial, tempo_final;
     double tempo_execucao;
 
@@ -76,21 +99,21 @@ int main(int argc, char const *argv[]) {
     vector<int> clausula;    
     int literal;
 
-    while (cin >> literal) {
+    while ( cin >> literal ) {
         clausula.clear();
-        while (literal) {
-            clausula.push_back(literal);
+        while ( literal ) {
+            clausula.push_back( literal );
             cin >> literal;
         }
-        formula.push_back(clausula);
+        formula.push_back( clausula );
     }
 
-    bool resultado = dpll(formula);
+    bool resultado = dpll( formula );
 
     tempo_final = clock();
     tempo_execucao = ((double)(tempo_final - tempo_inicial)) / CLOCKS_PER_SEC;
 
-    if (resultado) {
+    if ( resultado ) {
         cout << "d\tSATISFIABLE\n";
     } else {
         cout << "d\tUNSATISFIABLE\n";
