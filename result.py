ad0e4fb79bb2c2791bb6bec04671501cdb840489
mateusfranco <mateusfrancofs@hotmail.com>
2018-11-22 10:35:31 -0300

    result na versao final

--- result.py
+++ result.py
@@ -1,34 +1,41 @@
 from os import popen 
 import time
-Nclausulas =  int(input("numero de clausulas: "))
-popen("g++ gerador-clausulas.cpp -o gerador")
-popen("g++ dpll.cpp -o dpll")
-time.sleep(5)
-aux = "./gerador " + str(Nclausulas)
-popen(aux)
-time.sleep(5)
-aux = "./dpll <" + str(Nclausulas) +"-rainhas.cnf" 
-print(popen(aux).read())
+Nrainhas =  int(input("numero de rainhas: "))
+if(Nrainhas <= 0):
+    print("problema")
+    exit(1)
+elif(Nrainhas >= 1 and Nrainhas <= 3):
+    print("unsat")
+    exit(1)
+else:
+    popen("g++ gerador-clausulas.cpp -o gerador")
+    popen("g++ dpll.cpp -o dpll")
+    time.sleep(3)
+    aux = "./gerador " + str(Nrainhas)
+    popen(aux)
+    time.sleep(3)
+    aux = "./dpll <" + str(Nrainhas) +"-rainhas.cnf" 
+    print(popen(aux).read())
 
 
-import math
-texto = ""
-with open("valoracao.txt","r") as arq:
-    texto = arq.read()
+    import math
+    texto = ""
+    with open("valoracao.txt","r") as arq:
+        texto = arq.read()
 
-rainhas = texto.split(" ")
-import matplotlib.pyplot as plt
-nrainhas = math.sqrt(len(rainhas))
-nrainhas = int(nrainhas)
-for i in range(nrainhas):
-        #[x,x],[y,y] 
-    plt.plot([i,i],[0,nrainhas],color="black")
-    plt.plot([0,nrainhas],[i,i],color="black")
+    rainhas = texto.split(" ")
+    import matplotlib.pyplot as plt
+    nrainhas = math.sqrt(len(rainhas))
+    nrainhas = int(nrainhas)
+    for i in range(nrainhas):
+            #[x,x],[y,y] 
+        plt.plot([i,i],[0,nrainhas],color="black")
+        plt.plot([0,nrainhas],[i,i],color="black")
 
-for i in rainhas:
-    i = int(i)-1
-    if(i > 0):
-        x = i%nrainhas
-        y = i//nrainhas
-        plt.plot([x+0.5],[y+0.5],"ro",color="blue")
-plt.show()
+    for i in rainhas:
+        i = int(i)-1
+        if(i > 0):
+            x = i%nrainhas
+            y = i//nrainhas
+            plt.plot([x+0.5],[y+0.5],"ro",color="blue")
+    plt.show()
